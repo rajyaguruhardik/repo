88f73f27b4e3d74cd4c88cfa72cd2b6aa5622fef
rajyaguruhardik <rajyaguru.hardik123@gmail.com>
2023-03-29 18:23:02 +0530

    full

--- StockScanner/StockScanner/StockScanner/settings/base.py
+++ StockScanner/StockScanner/StockScanner/settings/base.py
@@ -6,11 +6,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+7ko1t71s&c9rpdr4o^rl#wv3vxz08svs!al@9g@%l+1@yj6v9'
 
 INSTALLED_APPS = [
-    # ...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'homepage',
+    'stock_scanner_app',
 ]
 
 MIDDLEWARE = [
-    # ...
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'StockScanner.urls'
